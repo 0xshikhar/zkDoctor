7fc1b6042a653e2950f6714fbe53247857a0cb16
0xShikhar <0xshikhar@gmail.com>
2023-11-06 00:11:33 +0530

    zk contract written

--- contracts/src/Add.ts
+++ contracts/src/Add.ts
@@ -1,4 +1,4 @@
-import { Field, SmartContract, state, State, method } from 'o1js';
+import { Field, SmartContract, state, State, method, MerkleWitness, PublicKey, Signature, PrivateKey } from 'o1js';
 
 /**
  * Basic Example
@@ -9,17 +9,72 @@ import { Field, SmartContract, state, State, method } from 'o1js';
  *
  * This file is safe to delete and replace with your own contract.
  */
+
+class MerkleWitness4 extends MerkleWitness(4) {
+
+}
 export class Add extends SmartContract {
-  @state(Field) num = State<Field>();
+
+  @state(Field) nextIndex = State<Field>();
+
+  @state(PublicKey) cpsoPublicKey = State<PublicKey>();
+
+  @state(Field) root = State<Field>();
 
   init() {
     super.init();
-    this.num.set(Field(1));
   }
 
-  @method update() {
-    const currentState = this.num.getAndAssertEquals();
-    const newState = currentState.add(2);
-    this.num.set(newState);
+  // called when contract is deployed
+  @method initState(cpsoPublicKey: PublicKey, initRoot: Field) {
+    this.cpsoPublicKey.set(cpsoPublicKey);
+    this.root.set(initRoot); // root of the merkle tree
+    this.nextIndex.set(Field(0));
   }
+
+  @method addDoctor(cpsoPrivateKey: PrivateKey, doctor: PublicKey, leafWitness: MerkleWitness4) {
+    // Circuit Assertion
+    const commitedPublicKey = this.cpsoPublicKey.get();
+    this.cpsoPublicKey.assertEquals(commitedPublicKey);
+
+    // Check public key is only excuting it -  by verifying the signature using private key
+    commitedPublicKey.assertEquals(cpsoPrivateKey.toPublicKey());
+
+    // Check witness
+    const initialRoot = this.root.get();
+    this.root.assertEquals(initialRoot);
+
+    this.nextIndex.assertEquals(leafWitness.calculateIndex());
+
+    // creating new root
+    const newRoot = leafWitness.calculateRoot(doctor.x);
+    this.root.set(newRoot);
+
+    // set new index
+    const currIndex = this.nextIndex.get();
+    this.nextIndex.assertEquals(currIndex);
+    this.nextIndex.set(currIndex.add(Field(1)));
+
+    // const index = this.nextIndex.get();
+    // const newRoot = this.root.get().update(index, doctor);
+    // this.root.set(newRoot);
+    // this.nextIndex.set(index + Field(1));
+  }
+
+  @method verifySickNote(doctorWitness: MerkleWitness4, doctorPubKey: PublicKey, signature: Signature, patientPubKey: PublicKey) {
+
+    this.root.assertEquals(doctorWitness.calculateRoot(doctorPubKey.x));
+
+    const ok = signature.verify(doctorPubKey, patientPubKey.toFields());
+    ok.assertTrue();
+
+
+    // const root = this.root.get();
+    // const index = doctorWitness.calculateIndex();
+    // const newRoot = doctorWitness.calculateRoot(patient.x);
+    // root.assertEquals(newRoot);
+    // const leaf = doctorWitness.getLeaf();
+    // leaf.assertEquals(sickNote);
+  }
+
 }
